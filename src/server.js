bea463d79d1c15ac2b452727b087983bc56a36de
Kingsman4451 <dadosh4451@gmail.com>
2022-08-27 13:49:12 +0500

    Give access control

--- src/server.js
+++ src/server.js
@@ -8,6 +8,11 @@ import  messageRouter from './routes/message.router.js'
 const PORT = process.env.PORT || 5000;
 
 let app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json())
 app.use(fileUpload())
 app.use(express.static(path.join(process.cwd(), 'src', 'uploads')))
